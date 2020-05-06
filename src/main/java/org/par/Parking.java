5d73007caf7f5de6f9f06686f0bd23d5705b9fa7
Lucas <lucas.dudek81@gmail.com>
2020-05-06 10:09:02 +0200

    Parking leaving ver 1

--- src/main/java/org/par/Parking.java
+++ src/main/java/org/par/Parking.java
@@ -1,5 +1,6 @@
 package org.par;
 
+import javax.lang.model.util.Types;
 import java.time.LocalDateTime;
 import java.util.*;
 
@@ -9,6 +10,7 @@ import static org.par.Parking.EngineType.*;
 public class Parking {
 
     private final Map<EngineType, List<String>> slots;
+    private final Set<Ticket> tickets = new HashSet<>();
 
     public Parking(Map<EngineType, List<String>> slots) {
         this.slots = slots.entrySet().stream().collect(toMap(Map.Entry::getKey, e -> new ArrayList<>(e.getValue())));
@@ -19,14 +21,20 @@ public class Parking {
             case HI_ELECTRIC:
             case ELECTRIC:
                 return assign(engineType)
-                        .map(s -> new Ticket(engineType, s));
+                        .map(s -> createTicket(engineType, s));
             case GAS:
             default:
                 return assign(GAS)
                         .or(() -> assign(ELECTRIC))
                         .or(() -> assign(HI_ELECTRIC))
-                        .map(s -> new Ticket(GAS, s));
+                        .map(s -> createTicket(GAS, s));
         }
+    }
+
+    private Ticket createTicket(EngineType engineType, String slot) {
+        var ticket = new Ticket(engineType, slot);
+        tickets.add(ticket);
+        return ticket;
     }
 
     private Optional<String> assign(EngineType t) {
@@ -35,6 +43,10 @@ public class Parking {
             return Optional.of(matchingSlots.remove(0));
         else
             return Optional.empty();
+    }
+
+    public boolean leave(Ticket ticket) {
+        return tickets.remove(ticket);
     }
 
     public enum EngineType {
@@ -52,6 +64,22 @@ public class Parking {
             this.id = UUID.randomUUID();
             this.engineType = engineType;
             this.issueTime = LocalDateTime.now();
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Ticket ticket = (Ticket) o;
+            return id.equals(ticket.id) &&
+                    engineType == ticket.engineType &&
+                    issueTime.equals(ticket.issueTime) &&
+                    slotId.equals(ticket.slotId);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(id, engineType, issueTime, slotId);
         }
     }
 
--- src/test/java/org/par/ParkingTest.java
+++ src/test/java/org/par/ParkingTest.java
@@ -4,6 +4,8 @@ import javafx.util.Pair;
 import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestFactory;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
@@ -18,19 +20,19 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.par.Parking.EngineType;
 import static org.par.Parking.EngineType.*;
 import static org.par.Parking.Ticket;
+import static org.par.ParkingTest.ParkingAssertions.assertRightSlotAssigned;
 
 public class ParkingTest {
-
-    private void assertRightSlotAssigned(Parking parking, Ticket ticket, EngineType engineType) {
-        var issued = parking.enter(engineType);
-        var expected = Optional.ofNullable(ticket);
-        assertEquals(expected.map(t -> t.engineType), issued.map(t -> t.engineType));
-        assertEquals(expected.map(t -> t.slotId), issued.map(t -> t.slotId));
-        issued.ifPresent(i -> {
-            assertTrue(i.issueTime.isBefore(LocalDateTime.now().plus(1, ChronoUnit.SECONDS)),
-                    "Non valid timestamp was issued");
-            assertNotEquals(expected.map(t -> t.id), issued.map(t -> t.id));
-        });
+    private static String generateName(EngineType engineType, int i) {
+        switch (engineType) {
+            case ELECTRIC:
+                return "E" + i;
+            case HI_ELECTRIC:
+                return "H" + i;
+            case GAS:
+            default:
+                return "G" + i;
+        }
     }
 
     @Test
@@ -44,25 +46,6 @@ public class ParkingTest {
         assertRightSlotAssigned(parking, null, GAS);
     }
 
-    private String generateName(EngineType engineType, int i) {
-        switch (engineType) {
-            case ELECTRIC:
-                return "E" + i;
-            case HI_ELECTRIC:
-                return "H" + i;
-            case GAS:
-            default:
-                return "G" + i;
-        }
-    }
-
-    private void assertParkingCapacity(Parking parking, EngineType engineType, int carsNumber) {
-        IntStream.range(0, carsNumber).forEach(i ->
-                assertRightSlotAssigned(parking, new Ticket(engineType, generateName(engineType, i)), engineType)
-        );
-        assertRightSlotAssigned(parking, null, engineType);
-    }
-
     @TestFactory
     Stream<DynamicTest> parkingCanHaveNumberOfSlotsConfigured() {
         var seed = System.currentTimeMillis();
@@ -70,19 +53,53 @@ public class ParkingTest {
         var tests = IntStream.range(0, 20)
                 .mapToObj(i -> Map.of(GAS, generator.next(), ELECTRIC, generator.next(), HI_ELECTRIC, generator.next()))
                 .iterator();
-        return DynamicTest.stream(tests, (input) -> "seed:" + seed + " input:" + input, this::assertParkingRespectsItsSize).parallel();
+        return DynamicTest.stream(tests, (input) -> "seed:" + seed + " input:" + input, ParkingAssertions::assertParkingRespectsItsSize).parallel();
     }
 
-    private void assertParkingRespectsItsSize(Map<EngineType, Integer> indexedSizes) {
-        BiFunction<EngineType, Integer, List<String>> namesGenerator =
-                (eng, size) -> IntStream.range(0, size).mapToObj(i -> generateName(eng, i)).collect(toList());
-        var slots = indexedSizes.entrySet()
-                .stream()
-                .map(e -> new Pair<>(e.getKey(), namesGenerator.apply(e.getKey(), e.getValue())))
-                .collect(toMap(Pair::getKey, Pair::getValue));
-        var parking = new Parking(slots);
-        indexedSizes.keySet().stream().sorted(Comparator.comparingInt(EngineType::ordinal).reversed())
-                .forEach(t -> assertParkingCapacity(parking, t, indexedSizes.get(t)));
+    @ParameterizedTest
+    @CsvSource({"ELECTRIC", "GAS", "ELECTRIC", "GAS", "HI_ELECTRIC"})
+    void youCanOnlyLeaveWhenPossessingValidTicket(EngineType engineType) {
+        var parking = new Parking(Map.of(engineType, List.of("A")));
+        assertFalse(parking.leave(new Ticket(engineType, "A")), "Spoofed ticket should not work.");
+        parking.enter(engineType).ifPresentOrElse(t -> {
+            assertTrue(parking.leave(t), "Issued ticket should work the first time.");
+            assertFalse(parking.leave(t), "Issued ticket should not work the second time.");
+        }, () -> {
+            throw new AssertionError("Ticket was not generated");
+        });
+    }
+
+    public static class ParkingAssertions {
+        static void assertRightSlotAssigned(Parking parking, Ticket ticket, EngineType engineType) {
+            var issued = parking.enter(engineType);
+            var expected = Optional.ofNullable(ticket);
+            assertEquals(expected.map(t -> t.engineType), issued.map(t -> t.engineType));
+            assertEquals(expected.map(t -> t.slotId), issued.map(t -> t.slotId));
+            issued.ifPresent(i -> {
+                assertTrue(i.issueTime.isBefore(LocalDateTime.now().plus(1, ChronoUnit.SECONDS)),
+                        "Non valid timestamp was issued");
+                assertNotEquals(expected.map(t -> t.id), issued.map(t -> t.id));
+            });
+        }
+
+        static void assertParkingCapacity(Parking parking, EngineType engineType, int carsNumber) {
+            IntStream.range(0, carsNumber).forEach(i ->
+                    assertRightSlotAssigned(parking, new Ticket(engineType, generateName(engineType, i)), engineType)
+            );
+            assertRightSlotAssigned(parking, null, engineType);
+        }
+
+        static void assertParkingRespectsItsSize(Map<EngineType, Integer> indexedSizes) {
+            BiFunction<EngineType, Integer, List<String>> namesGenerator =
+                    (eng, size) -> IntStream.range(0, size).mapToObj(i -> generateName(eng, i)).collect(toList());
+            var slots = indexedSizes.entrySet()
+                    .stream()
+                    .map(e -> new Pair<>(e.getKey(), namesGenerator.apply(e.getKey(), e.getValue())))
+                    .collect(toMap(Pair::getKey, Pair::getValue));
+            var parking = new Parking(slots);
+            indexedSizes.keySet().stream().sorted(Comparator.comparingInt(EngineType::ordinal).reversed())
+                    .forEach(t -> assertParkingCapacity(parking, t, indexedSizes.get(t)));
+        }
     }
 
 
